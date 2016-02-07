305a0844b3ef9e7654a00018e49eb7249e28d91a
Ben Limmer <ben@benlimmer.com>
2016-02-07 14:37:06 -0700

    Serve up images from giphy.

--- app.js
+++ app.js
@@ -7,6 +7,38 @@ const io = require('socket.io')(server);
 
 app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+var giphy = require('giphy-api')();
+app.get('/api/v1/gifs', function(req, res) {
+  const opts = {
+    limit: 25,
+    rating: 'pg-13',
+  };
+
+  giphy.trending().then(function(gifs) {
+    const response = {
+      data: []
+    };
+    gifs.data.forEach(function(gif) {
+      response.data.push({
+        type: 'gif',
+        id:   gif.id,
+        attributes: {
+          url:  gif.images.original.url,
+        }
+      });
+    });
+
+    res.json(response);
+  }).catch(function() {
+    res.sendStatus(503); // ¯\_(ツ)_/¯
+  });
+});
 
 app.post('/api/v1/notify-upgrade', function(req, res) {
   const token = req.body.token;
